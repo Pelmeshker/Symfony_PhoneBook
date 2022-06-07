c95071489d8e23ae48d5e845ddbf52d75a887706
Pelmeshker <pelmeshker@gmail.com>
2022-06-07 19:55:16 +0300

    Пустой проект с настроенным докером

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    #[Route("/")]
+
+    public function showMain(): Response
+    {
+       return $this->render('Index.html.twig');
+    }
+}
